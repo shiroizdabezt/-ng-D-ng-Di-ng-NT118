7700aba2eed015481fa328aff2a9c9d1d12f736e
Shiroizdabezt <codykhoanguyen@gmail.com>
2023-12-06 22:12:53 +0700

    lab5

---
+++ Lab5_2/Bai2/app/src/main/java/com/example/bai2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.bai2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
